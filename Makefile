04b3b57598a83ba42d91540fd41a42d192742659
ArteSpanjers <artespanjers@hotmail.com>
2024-02-07 10:31:46 +0000

    adding initial framework for project

---
+++ Makefile
@@ -0,0 +1,15 @@
+install: 
+		pip install --upgrade pip&&\
+		pip install -r requirements.txt
+
+test:
+		python -m pytest -s -vv test_hello.py
+
+format:
+		black *.py
+
+lint:
+		pylint --disable=R,C hello.py&&\
+		flake8 *.py
+
+all: install lint test format
---
+++ hello.py
@@ -0,0 +1,23 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# Testing for something that is not causing an error, but is bad practice
+# variable = 10
+# variable = variable
+
+# Testing for a mistake in the code
+# ans =
+
+
+class employee_salary:
+
+    def __init__(self):
+        self.salary = 0
+
+    def ReturnSalary(self):
+        return self.salary
